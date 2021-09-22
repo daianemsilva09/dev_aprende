f6dbf8eb4c77f31d4e5e639ce3f526bc0751b876
Daiane Silva <daianemsilva09@gmail.com>
2021-09-22 15:13:44 -0300

    templateLiteral

---
+++ javascript/parte3/templateLiteral.js
@@ -0,0 +1,11 @@
+//objeto{}
+//boolean true, false
+//string '', ""
+//Template ``
+
+const outra = `Oi isso é a minha 'primeira' mensagem`;
+
+const nome = 'Rafael'
+
+const email = `Olá ${nome} ${2+2}
+obrigado`
