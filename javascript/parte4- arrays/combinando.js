ac78e7b8258e07207966afe764d445c42965c961
Daiane Silva <daianemsilva09@gmail.com>
2021-09-23 10:26:43 -0300

    combinado

---
+++ javascript/parte4- arrays/combinando.js
@@ -0,0 +1,10 @@
+const numeros = [1,2,3,4,5];
+
+const combinado = numeros.join('.');
+console.log(combinado);
+
+const frase = "ola bem vindo";
+const resultado = frase.splint(' ');
+console.log(resultado);
+
+console.log(resultado.join('-'));
