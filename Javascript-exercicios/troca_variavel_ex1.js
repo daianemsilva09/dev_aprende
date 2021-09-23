8538d4f2dffbf61682946c2273d33d8b103a5bea
Daiane Silva <daianemsilva09@gmail.com>
2021-09-23 09:43:35 -0300

    arrow function

--- Javascript-exercicios/troca_variavel_ex1.js
+++
@@ -1,9 +0,0 @@
-let a = 'vermelho'
-let b = 'azul'
-let c = b
-b = a
-a = c
-
-
-console.log(a)
-console.log(b)
--- javascript/parte4/encontrarReferencia.js
+++ javascript/parte4/encontrarReferencia.js
@@ -9,3 +9,12 @@ const marca = marcas.find(function(marca){
 
 
 console.log(marca);
+// arrow functions
+
+
+console.log(marcas.find((marca)=> marca.nome === 'a'));
+
+
+
+
+
